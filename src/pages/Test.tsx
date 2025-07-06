6a504721953bb1ca8d0c8a9ef5fcd4b553c9205b
KingSeokJin <sjin7629@gmail.com>
2025-07-07 01:10:24 +0900

    [fix] 테스트 코드 수정

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,7 +2,7 @@ const Test = () => {
   return (
     <div className="h-[240px] w-full bg-sky-400 text-xl text-white">
       이 공간은 라우팅 테스트용 코드이며, 추후 루트페이지(홈 페이지) 개발 완료시 대체 예정입니다.
-      제발 cicd 테스트 통과해줄래? 제발 제발 제발 1:01
+      제발 cicd 테스트 통과해줄래? 제발 제발 제발 1:10
     </div>
   );
 };
