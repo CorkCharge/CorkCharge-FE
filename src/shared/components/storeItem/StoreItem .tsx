afb05434750d40b9ec4bbc35ff711dfd762eca04
KingSeokJin <sjin7629@gmail.com>
2025-07-15 00:05:42 +0900

    Design: StoreItem 구현 완료

--- src/shared/components/storeItem/StoreItem .tsx
+++ src/shared/components/storeItem/StoreItem .tsx
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+import Circle from './circle.svg';
+import Checked from './checked_circle.svg';
+import Bg from './bg.svg';
+
 const StoreItem = () => {
-  return <div>StoreItem </div>;
+  const [isChecked, setIsChecked] = useState(false);
+  const toggleChecked = () => {
+    setIsChecked((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={`relative z-10 flex h-[88px] w-[361px] flex-row items-center rounded-[16px] ${
+        isChecked ? '' : 'bg-[#F3F3F6]'
+      }`}
+    >
+      {isChecked && (
+        <img
+          src={Bg}
+          alt="선택된 배경"
+          className="absolute left-0 top-0 -z-10 h-full w-full rounded-[16px] object-cover"
+        />
+      )}
+      <div className="flex w-[310px] flex-col">
+        <div
+          className={`ml-[16px] text-[20px] font-[700] ${isChecked ? 'text-white' : 'text-black'}`}
+        >
+          깍둑 - 경희대점
+        </div>
+        <div
+          className={`ml-[16px] text-[14px] font-[500] ${isChecked ? 'text-white' : 'text-black'}`}
+        >
+          서울 광진구 아차산로 24 2층
+        </div>
+      </div>
+      {/* 라디오 버튼 */}
+      <img
+        src={isChecked ? Checked : Circle}
+        alt="라디오버튼"
+        className="cursor-pointer"
+        onClick={toggleChecked}
+      />
+    </div>
+  );
 };
 
 export default StoreItem;
