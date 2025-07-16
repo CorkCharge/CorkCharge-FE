2aae645f23ae7ee525d8f49c91b7155f01c4cd86
TakioN <jungbeen0724@gmail.com>
2025-07-16 20:47:51 +0900

    feat: add log-in select pages

--- src/pages/onBoarding/PreferSelect.tsx
+++ src/pages/onBoarding/PreferSelect.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+
+import PreferDrink from '@/shared/components/onBoarding/PreferDrink';
+import PreferMenu from '@/shared/components/onBoarding/PreferMenu';
+
 function PreferSelect() {
-  return <div></div>;
+  const [page, setPage] = useState(1);
+
+  return (
+    <>
+      {page === 1 && <PreferDrink onNext={setPage} />}
+      {page === 2 && <PreferMenu />}
+    </>
+  );
 }
 
 export default PreferSelect;
