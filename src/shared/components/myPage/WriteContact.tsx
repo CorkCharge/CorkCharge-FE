52d3c033b054faacbaf456bc81be38fba5a3c9f9
TakioN <jungbeen0724@gmail.com>
2025-08-06 03:10:30 +0900

    Style: add contact type selection

--- src/shared/components/myPage/WriteContact.tsx
+++ src/shared/components/myPage/WriteContact.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+
 import { Input } from '../common/Input';
 import camera from '@/shared/components/myPage/images/camera.png';
 
+const TYPE = ['콜키지 정보 오류', '가게 정보 오류', '기타'];
+
 function WriteContact() {
+  const [contactType, setContactType] = useState(0);
+  const renderType = () =>
+    TYPE.map((type, idx) => (
+      <span
+        className={`rounded-[20px] px-4 py-2 ${idx === contactType ? 'bg-[var(--primary)] text-white' : 'bg-[var(--gray-1)] text-[var(--gray-8)]'}`}
+        onClick={() => setContactType(idx)}
+      >
+        {type}
+      </span>
+    ));
   return (
     <>
       <Input placeholder="문의 제목을 입력해주세요" className="mt-10" />
+      <div className="mt-3 flex gap-1 px-2 text-sm font-medium">{renderType()}</div>
+      {/* <div className="mt-4 px-2 text-sm font-medium">
+        <span className="rounded-[20px] bg-[var(--gray-1)] px-4 py-2">콜키지 정보 오류</span>
+        <span>가게 정보 오류</span>
+        <span>기타</span>
+      </div> */}
       <textarea
-        className="mt-5 h-[300px] w-full resize-none rounded-2xl bg-[var(--gray-1)] px-8 py-5"
+        className="mt-3 h-[300px] w-full resize-none rounded-2xl bg-[var(--gray-1)] px-8 py-5"
         placeholder="문의 내용을 입력해주세요"
         name="content"
       ></textarea>
@@ -14,7 +34,7 @@ function WriteContact() {
         <img src={camera} className="h-[22px] w-[25px]" />
         사진 첨부하기
       </button>
-      <button className="fixed bottom-9 left-10 right-10 h-[48px] rounded-[10px] bg-[var(--primary)] font-bold text-white">
+      <button className="fixed bottom-9 left-10 right-10 mx-auto h-[48px] max-w-[480px] rounded-[10px] bg-[var(--primary)] font-bold text-white">
         등록하기
       </button>
     </>
