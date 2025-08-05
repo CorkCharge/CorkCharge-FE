ccd4e8a45586c9b311566590b5f0a5ab76b8b91d
TakioN <jungbeen0724@gmail.com>
2025-08-06 04:19:58 +0900

    Feat: modifiable star-rate in review page

---
+++ src/shared/components/myPage/ReviewModals.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+
+import Modal from '../common/Modal';
+import { StarRate } from './StarRate';
+
+import camera from '@/shared/components/myPage/images/camera.png';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const ShareModal = ({ isOpen, onClose }: ModalProps) => {
+  return (
+    <Modal isOpen={isOpen} className="bg-[rgba(255,255,255,0.8)]">
+      <h3 className="mb-2 text-center font-bold text-[var(--gray-8)]">후기를 공유하시겠습니까?</h3>
+      <p className="mb-1 text-center text-sm font-medium text-[var(--gray-8)]">깍둑 건대점</p>
+      <p className="text-center text-[10px] text-[var(--gray-6)]">2025.07.24 작성</p>
+      <div className="mt-5 flex w-full gap-1">
+        <button
+          className="h-[44px] flex-1 rounded-[10px] bg-white font-bold text-[var(--gray-7)]"
+          onClick={onClose}
+        >
+          취소
+        </button>
+        <button className="h-[44px] flex-1 rounded-[10px] bg-[rgba(144,33,70,0.15)] font-bold text-[var(--primary)]">
+          공유
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export const ModifyModal = ({ isOpen, onClose }: ModalProps) => {
+  const [reviewInput, setReviewInput] = useState('');
+
+  return (
+    <Modal isOpen={isOpen} className="bg-[rgba(255,255,255,0.8)] text-center">
+      <h3 className="mb-2 text-center text-2xl font-bold text-[var(--gray-8)]">리뷰 수정</h3>
+      <StarRate rate={3} isEditable={true} className="mb-7 justify-center" />
+      <div className="relative mb-5 h-[150px] rounded-ee-[30px] rounded-ss-[30px] bg-white">
+        <textarea
+          className="h-full w-full resize-none rounded-ee-[30px] rounded-ss-[30px] p-5 pr-10 pt-3 text-xs"
+          value={reviewInput}
+          onChange={(e) => setReviewInput(e.target.value)}
+        />
+        <span className="absolute right-2 top-2">&times;</span>
+      </div>
+
+      <button className="flex h-[44px] w-full items-center justify-center gap-2 rounded-xl bg-[var(--gray-1)] text-sm font-bold text-[var(--primary)]">
+        <img src={camera} className="h-[22px] w-[25px]" />
+        사진 첨부하기
+      </button>
+      <div className="mt-5 flex w-full gap-1">
+        <button
+          className="h-[44px] flex-1 rounded-[10px] bg-[var(--gray-1)] font-bold text-[var(--gray-7)]"
+          onClick={onClose}
+        >
+          취소
+        </button>
+        <button
+          className={`h-[44px] flex-1 rounded-[10px] font-bold ${reviewInput ? 'bg-[var(--primary)] text-white' : 'bg-[var(--gray-1)] text-[var(--gray-7)]'}`}
+        >
+          완료
+        </button>
+      </div>
+    </Modal>
+  );
+};
+
+export const DeleteModal = ({ isOpen, onClose }: ModalProps) => {
+  return (
+    <Modal isOpen={isOpen} className="bg-[rgba(255,255,255,0.8)]">
+      <h3 className="mb-2 text-center font-bold text-[var(--gray-8)]">후기를 삭제하시겠습니까?</h3>
+      <p className="mb-1 text-center text-sm font-medium text-[var(--gray-8)]">깍둑 건대점</p>
+      <p className="text-center text-[10px] text-[var(--gray-6)]">2025.07.24 작성</p>
+      <div className="mt-5 flex w-full gap-1">
+        <button
+          className="h-[44px] flex-1 rounded-[10px] bg-white font-bold text-[var(--gray-7)]"
+          onClick={onClose}
+        >
+          취소
+        </button>
+        <button className="h-[44px] flex-1 rounded-[10px] bg-[rgba(144,33,70,0.15)] font-bold text-[var(--primary)]">
+          삭제
+        </button>
+      </div>
+    </Modal>
+  );
+};
