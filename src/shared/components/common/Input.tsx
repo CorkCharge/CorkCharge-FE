753c750f1e56c8b07d86e2152e9a3a8b0bc716f7
TakioN <jungbeen0724@gmail.com>
2025-08-06 18:15:39 +0900

    Feat: connect api in modifying profile pages

--- src/shared/components/common/Input.tsx
+++ src/shared/components/common/Input.tsx
@@ -9,6 +9,9 @@ interface InputProps {
   onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   value?: string;
   onSearch?: () => void;
+  imgSrc?: string;
+  onImgClick?: () => void;
+  imgClassName?: string;
 }
 
 export const Input = ({ className, placeholder, onChange, value }: InputProps) => {
@@ -58,6 +61,38 @@ export const SearchInput = ({
         src={search}
         className="absolute right-10 top-1/2 size-[18px] -translate-y-1/2"
         onClick={onSearch}
+      />
+    </div>
+  );
+};
+
+export const ImageInput = ({
+  imgSrc,
+  className,
+  placeholder,
+  onChange,
+  value,
+  onImgClick,
+  imgClassName,
+}: InputProps) => {
+  return (
+    <div
+      className={cn(
+        'relative flex h-[46px] items-center rounded-ee-full rounded-ss-full bg-[var(--gray-1)] pl-8 pr-[65px]',
+        className
+      )}
+    >
+      <input
+        className="w-full bg-transparent"
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+      />
+
+      <img
+        src={imgSrc}
+        className={cn('absolute right-10 top-1/2 size-[18px] -translate-y-1/2', imgClassName)}
+        onClick={onImgClick}
       />
     </div>
   );
