7d8d174a3a3d79accf20ddf026bc10656e6aabdd
TakioN <jungbeen0724@gmail.com>
2025-08-18 15:56:40 +0900

    chore: remove unused temporary code

--- src/shared/components/layout/Footer.tsx
+++ src/shared/components/layout/Footer.tsx
@@ -9,21 +9,6 @@ const Footer = () => {
   const navigate = useNavigate();
   const { footerProps, setFooterProps } = useFooterPropsStore();
 
-  useEffect(() => {
-    apiClient
-      .post(
-        'suggestions',
-        {
-          title: '김창훈 포차 콜키지 비용 오류',
-          content: '김창훈 포차 식당이 콜키지 비용이 공지된 내용과 달랐어요',
-          category: 'CORKAGE_ERROR',
-        },
-        { params: { userId: 1 } }
-      )
-      .then((res) => console.log(res))
-      .catch((e) => console.log(e));
-  }, []);
-
   const setPropsIdx = (idx: number, path: string) => {
     if (footerProps === idx || !path) return; // 이미 해당 페이지에 있는경우
 
