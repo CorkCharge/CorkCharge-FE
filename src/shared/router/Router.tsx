f7107d95495e3ac27f6354360b35bc40e3ddd8b1
KingSeokJin <sjin7629@gmail.com>
2025-08-19 20:03:25 +0900

    Feat: 해주세요 연동완료

--- src/shared/router/Router.tsx
+++ src/shared/router/Router.tsx
@@ -83,7 +83,7 @@ const router = createBrowserRouter([
         element: <AlreadyRegistered />,
       },
       {
-        path: 'doit/request',
+        path: 'doit/request/:restaurantId',
         element: <Request />,
       },
       {
