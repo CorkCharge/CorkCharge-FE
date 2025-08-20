74e132a5c143ce6b8f891db0348e0dfe93000059
G4hyun <sally_0113@naver.com>
2025-08-20 19:43:38 +0900

    fix: Tip props 수정

--- src/pages/keep/Tip.tsx
+++ src/pages/keep/Tip.tsx
@@ -14,18 +14,12 @@ const Tip = () => {
     <div className="flex flex-col items-center justify-center">
       <div className="relative">
         <TipArticle
-          category="페어링 큐레이션"
-          mainTitle="삼겹살과 페어링하기 좋은 주류 츄천"
-          // subTitle="🥓삼겹살과 주류의\nPERFECT 조합"
-          subTitle={`🥓삼겹살과 주류의
-        PERFECT 조합`}
-          info={`그 맛은 두 배가 되죠.
-        “소주만 먹기엔 뭔가 아쉽다…”
- 
-        하셨던 분들께, 오늘은 삼겹살과 찰떡같이 어울리는 주류 조합을 소개해드립니다.
-
-    고기 한 점에 술 한 잔, 
-    그 조화가 완벽해지는 순간을 위해 고른 추천 리스트`}
+          tipId={1}
+          title="삼겹살과 페어링하기 좋은 주류 츄천" //'와인 테스트';
+          content="fdssv" //'참석 전 물병, 스파이월러, 노트와 펜을 챙기면 편해요. 드레스 코드는 ‘스마트 캐주얼’인 경우가 많으니 미리 확인하시고, 일찍 도착해 무료 스낵도 즐겨보세요.';
+          tipCategory="페어링 큐레이션" //'EVENT';
+          imageUrls={['fsef', 'fef']}
+          createdAt="2025-08-07T21:40:31.530027"
         />
         <div className="absolute top-0">
           <div className="flex h-[48px] w-[393px] items-center justify-between pl-4 pr-4">
