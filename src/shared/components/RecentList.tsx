13d77d835c3fc6eae0377a2deb1558e316092922
KingSeokJin <sjin7629@gmail.com>
2025-08-21 17:30:43 +0900

    fix

--- src/shared/components/RecentList.tsx
+++ src/shared/components/RecentList.tsx
@@ -5,32 +5,32 @@ interface recentSearchProps {
   searchValue?: string;
   setSearchValue?: (value: string) => void;
 
-  directSearch?: (value : string) => void;
+  directSearch?: (value: string) => void;
 }
 
 const RecentList = ({ setSearchValue, directSearch }: recentSearchProps) => {
   return (
     <div className="mt-4 flex flex-col items-center">
       <RecentSearch
-        text="레스토랑 드 모간"
+        text="모다미육"
         // searchValue={searchValue}
         directSearch={directSearch}
         setSearchValue={setSearchValue}
       />
       <RecentSearch
-        text="수제버거 마트집"
+        text="만경상회"
         // searchValue={searchValue}
         directSearch={directSearch}
         setSearchValue={setSearchValue}
       />
       <RecentSearch
-        text="한우네 삼겹살"
+        text="재즈라운지"
         directSearch={directSearch}
         // searchValue={searchValue}
         setSearchValue={setSearchValue}
       />
       <RecentSearch
-        text="사케에 라면집"
+        text="브네"
         directSearch={directSearch}
         // searchValue={searchValue}
         setSearchValue={setSearchValue}
