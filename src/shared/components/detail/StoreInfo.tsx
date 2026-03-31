562fab4f11b5f1b06d4c468897d7cfd0c36663cd
TakioN <jungbeen0724@gmail.com>
2026-03-31 21:19:27 +0900

    fix: add copy-complete modal in store info page (#2603291630)

--- src/shared/components/detail/StoreInfo.tsx
+++ src/shared/components/detail/StoreInfo.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 import type { RestaurantInfo } from '@/shared/apis/restaurant/corkageApi';
 
+import check from './assets/check.svg';
+
 const StoreInfo = ({ restaurant }: { restaurant: RestaurantInfo }) => {
+  const [isCopiedModalOpen, setIsCopiedModalOpen] = useState(false);
+
   const handleCopy = async (copied: string) => {
     try {
       await navigator.clipboard.writeText(copied);
+      setIsCopiedModalOpen(true);
+      setTimeout(() => setIsCopiedModalOpen(false), 1000);
     } catch (e) {
       console.error('복사 실패: ' + e);
     }
@@ -54,6 +62,15 @@ const StoreInfo = ({ restaurant }: { restaurant: RestaurantInfo }) => {
           )}
         </div>
       </div>
+
+      {/* 복사완료 모달 */}
+      {isCopiedModalOpen && (
+        <div className="fixed inset-0 z-50 flex justify-center bg-black/50">
+          <div className="absolute top-12 flex h-12 w-[125px] items-center justify-center rounded-xl bg-white p-6 font-semibold text-[var(--primary)] shadow-lg">
+            <img src={check} />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
